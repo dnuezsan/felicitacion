cf77f4c4890fd05ca4c7448594406f81e8489882
2DAW06 <2DAW06@esvirgua.es>
2021-12-14 10:10:52 +0100

    modificacion js

--- felicitacion/js/script.js
+++ felicitacion/js/script.js
@@ -8,9 +8,8 @@ function iniciar() {
     //controlPeli()
 
     let prueba = pagIzquierda.textContent = 'HAZ CLICK AQUÍ'
-    
 
-    pagIzquierda.onclick = escribir
+    eleccionParrafo()
 
     robot.style.visibility = 'hidden'
 
@@ -105,8 +104,8 @@ function fabricación() {
 }
 
 //Texto
-function escribir() {
-    let texto = pagina1.split('')
+function escribir(entrada) {
+    /* let texto = pagina1.split('')
     let i = 0
     let parrafo = document.createElement('p')
     document.getElementById('pagIzquierda').appendChild(parrafo)
@@ -115,9 +114,30 @@ function escribir() {
        let letra = pagIzquierda.textContent = pagIzquierda.innerHTML + texto[i]
         letra = 'black'
         i++
-    }, 10);
-    
+    }, 50); */
+
+    let texto = entrada.split('')
+    let i = 0
+    pagIzquierda.textContent = ''
+    texto.forEach(element => {
+        setInterval(() => {
+            let letra = pagIzquierda.textContent = pagIzquierda.innerHTML + element
+            i++
+        }, 50);
+    });
+
 }
+
+function eleccionParrafo() {
+    let i = 0
+    let array = [prueba1, prueba2]
+    pagIzquierda.onclick = () => {
+        escribir(array[i])
+    }
+}
+
+let prueba1 = 'gdfgjrtklñj'
+let prueba2 = '22222222222222'
 
 let pagina1 = `Hace mucho, mucho tiempo; en una tierra lejana nació un niño especial. Antiguas profecías hablaban de que ese niño traería el reino de Dios a los hombres, y tres
 grandes reyes magos de oriente guiados por una estrella vinieron a recibirlo con tres obsequios: oro incieso y mirra. El niño creció y realizó obras y prodigios que cambiaron el mundo
